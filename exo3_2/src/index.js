9d9f7d6b0f9ea52ba1b36f4e22706c2e14654fc0
wezerg <vivian.grimaux@hotmail.fr>
2022-04-29 10:24:26 +0200

    Modification des chemin de fichier Utilisation de la librairie "path" plutot qu'une concaténation simple

--- exo3_2/src/index.js
+++ exo3_2/src/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
+const path = require('path');
 // Impression du fichier txt
-console.log(fs.readFileSync(__dirname + '/log.txt', {encoding: 'utf-8'}));
+console.log(fs.readFileSync(path.join(__dirname, 'log.txt'), {encoding: 'utf-8'}));
 // Impression du fichier html
-console.log(fs.readFileSync(__dirname + '/../assets/index.html', {encoding: 'utf-8'}));
+console.log(fs.readFileSync(path.join(__dirname, '../assets/index.html'), {encoding: 'utf-8'}));
